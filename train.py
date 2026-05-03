0f3622a5e177525e3c1d95ee757dac116ec4f410
sai-vuruma <svuruma@email.sc.edu>
2026-05-03 13:12:22 -0400

    Use midpoint residual shift

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ class WeightedConformalShiftRegressor:
     def predict(self, X):
         base_pred = self.model_.predict(X)
         midpoint_shift = 0.5 * (self.low_q_ + self.high_q_)
-        return base_pred + 0.5 * self.bias_ + 0.5 * midpoint_shift
+        return base_pred + midpoint_shift
 
 
 t_start = time.time()
