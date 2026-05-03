fe6bdcb9ff3d5134f29d9f3e3d2fc7ad6fab9fd4
sai-vuruma <svuruma@email.sc.edu>
2026-05-03 13:14:16 -0400

    Scale midpoint shift to 1.5

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ class WeightedConformalShiftRegressor:
     def predict(self, X):
         base_pred = self.model_.predict(X)
         midpoint_shift = 0.5 * (self.low_q_ + self.high_q_)
-        return base_pred + 1.25 * midpoint_shift
+        return base_pred + 1.5 * midpoint_shift
 
 
 t_start = time.time()
